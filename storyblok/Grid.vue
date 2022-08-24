14bf3a001ec8d624421fbe30ec3523bca8213344
Manuel Schröder <manuel.schroeder@storyblok.com>
2022-08-24 16:31:45 +0200

    part-1

--- storyblok/Grid.vue
+++ storyblok/Grid.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     v-editable="blok"
-    class="container mx-auto grid md:grid-cols-3 gap-16 place-items-center"
+    class="container mx-auto grid md:grid-cols-3 gap-12 my-12 place-items-center"
   >
     <StoryblokComponent
       v-for="blok in blok.columns"
