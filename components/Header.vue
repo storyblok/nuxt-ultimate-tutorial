b5113259ed79270c9ed0276dfb8f9fc0175dd92b
Manuel Schröder <manuel.schroeder@storyblok.com>
2022-08-26 10:39:07 +0200

    update Header.vue

--- components/Header.vue
+++ components/Header.vue
@@ -1,16 +1,26 @@
+<script setup>
+const storyblokApi = useStoryblokApi()
+const { data } = await storyblokApi.get('cdn/stories/config', {
+  version: 'draft',
+  resolve_links: 'url',
+})
+
+const headerMenu = ref(null)
+headerMenu.value = data.story.content.header_menu
+</script>
+
 <template>
   <header class="w-full h-24 bg-[#f7f6fd]">
     <div class="container h-full mx-auto flex items-center justify-between">
       <NuxtLink to="/">
         <h1 class="text-[#50b0ae] text-3xl font-bold">Storyblok Nuxt</h1>
       </NuxtLink>
-      <nav>
+      <nav v-if="headerMenu">
         <ul class="flex space-x-8 text-lg font-bold">
-          <li>
-            <NuxtLink to="/blog" class="hover:text-[#50b0ae]">Blog</NuxtLink>
-          </li>
-          <li>
-            <NuxtLink to="/about" class="hover:text-[#50b0ae]">About</NuxtLink>
+          <li v-for="blok in headerMenu" :key="blok._uid">
+            <NuxtLink :to="blok.link.cached_url" class="hover:text-[#50b0ae]">
+              {{ blok.link.story.name }}
+            </NuxtLink>
           </li>
         </ul>
       </nav>
