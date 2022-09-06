0ab3535bbe8bd549ed9d75bf59a7a595603d4601
Manuel Schröder <manuel.schroeder@storyblok.com>
2022-09-06 13:13:22 +0200

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   content: ['storyblok/**/*.{vue,js}', 'components/**/*.{vue,js}', 'pages/**/*.vue'],
+  theme: {
+    fontFamily: {
+      sans: 'Roboto, sans-serif',
+    },
+  },
 }
