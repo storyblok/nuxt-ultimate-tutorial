15068c086886f03ac75970679d12bc382f309e62
Manuel Schröder <manuel.schroeder@storyblok.com>
2022-09-06 13:12:03 +0200

    update tailwind config

--- storyblok/Page.vue
+++ storyblok/Page.vue
@@ -1,10 +1,6 @@
 <template>
-  <div v-editable="blok" class="font-[Roboto] px-4">
-    <StoryblokComponent
-      v-for="blok in blok.body"
-      :key="blok._uid"
-      :blok="blok"
-    />
+  <div v-editable="blok" class="px-4">
+    <StoryblokComponent v-for="blok in blok.body" :key="blok._uid" :blok="blok" />
   </div>
 </template>
 
