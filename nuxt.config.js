73443671c717617c9a2549b1294bfd283d3dbe5d
Dawntraoz <albasilventefu@gmail.com>
2023-01-24 14:48:34 +0100

    docs: add region as example

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,9 @@ export default defineNuxtConfig({
       '@storyblok/nuxt',
       {
         accessToken: 'W1vLyxT5rQ15jBpANjnv0gtt',
+        // apiOptions: {
+        //   region: "us" // When creating your space in US region (EU default)
+        // }
       },
     ],
     '@nuxtjs/tailwindcss',
