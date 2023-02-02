c098fcf9a16c4f5da7fcdf7765e2428e25f476bb
sebbejohansson <sebbe.johansson1995@gmail.com>
2023-02-02 20:29:39 +0100

    Begins work to add prerenderer that takes i18n into account.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,11 @@
+import { NuxtI18nOptions } from '@nuxtjs/i18n';
+
+const i18nConfig = {
+  strategy: 'prefix_except_default',
+  locales: ['en', 'es'],
+  defaultLocale: 'en', // default locale
+} as NuxtI18nOptions;
+
 export default defineNuxtConfig({
   // ssr: false,
   css: ['@/assets/css/roboto.css'],
@@ -11,12 +19,17 @@ export default defineNuxtConfig({
     '@nuxtjs/tailwindcss',
     '@nuxtjs/i18n',
   ],
-  i18n: {
-    strategy: 'prefix_except_default',
-    locales: ['en', 'es'],
-    defaultLocale: 'en', // default locale
-  },
+  i18n: i18nConfig,
   generate: {
     routes: ['/es'],
+  },
+  hooks: {
+    async 'nitro:config'(nitroConfig) {
+      console.log(i18nConfig);
+      // if (nitroConfig.dev) { return; }
+      // ..Async logic..
+      // const dynamicRoutes = await useStoryblokRawFetchDynamicRoutes(process.env.STORYBLOK_API_TOKEN as string);
+      // nitroConfig?.prerender?.routes?.push(...dynamicRoutes);
+    },
   },
 });
